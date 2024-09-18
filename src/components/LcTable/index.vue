589d0cb56088ccd9ad850c005d4ed2dd9577e270
苏昊远 <1109069349@qq.com>
2024-09-18 18:18:19 +0800

    url获取数据

--- src/components/LcTable/index.vue
+++ src/components/LcTable/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="lc-table">{{ tableData }}
-        <el-table border :data="tableData" style="width: 100%">
+    <div class="lc-table">
+        <el-table border :data="showTableData" style="width: 100%">
             <slot></slot>
         </el-table>
     </div>
@@ -38,20 +38,33 @@ const props = defineProps({
     }
 });
 
-const showTableData = [];
+// 展示的表格数据
+const showTableData = ref([]);
+// 将表格数据赋值
+const getTableData = (type, reset) => {
+    // reset为true时，清空表格数据重新赋值，否则直接追加数据
+    if (reset) showTableData.length = 0;
+    // 使用url请求数据
+    if (type === 'url') {
+        request({
+            url: props.tableUrl,
+            method: props.requestMethod,
+            data: props.requestData,
+        }).then(res => {
+            // 使用dataHandleFn处理数据，默认不处理
+            showTableData.value.push(...props.dataHandleFn(res.data));
+        }).catch(err => {
+            console.log(err);
+        });
+    // 通过父组件传递tableData直接添加数据
+    } else if (type === 'data') {
+        showTableData.value.push(...props.tableData);
+    }
+}
 
 onMounted(() => {
-    if(props.tableUrl === '' || props.tableData.length > 0) return;
-    console.log(1);
-    request({
-        url: props.tableUrl,
-        method: props.requestMethod,
-        data: props.requestData,
-    }).then(res => {
-        // console.log(props.dataHandleFn(1));console.log(res.data);
-        props.tableData.push(...res.data);
-        console.log(props.tableData);
-    });
+    if (props.tableUrl) getTableData('url', true);
+    if (props.tableData.length > 0) getTableData('data', true);
 });
 
 </script>
